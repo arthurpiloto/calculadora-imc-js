05a5f7e40fa9614c7355593e0f588fbfa34f0ffd
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-10 08:20:15 -0300

    html e css feitos / js em progresso

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 `use strict`
+
+function mostrarResultado () {
+    const nome = document.getElementById(`nome`).value
+    const altura = document.getElementById(`altura`).value
+    const peso = document.getElementById(`peso`).value
+    const imcForm = document.getElementById(`imc-form`)
+
+    console.log(imcForm.reportValidity())
+
+    console.log(`nome: ${nome}`)
+    console.log(`peso: ${peso}`)
+    console.log(`altura: ${altura}`)
+    
+}
+
+const calcular = document.getElementById(`calcular`).addEventListener(`click`, mostrarResultado)
