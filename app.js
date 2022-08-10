47bc1c0aee3abfaea7b0881dd6d25cff2cb411ef
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-10 09:24:50 -0300

    calculadora finalizada

--- app.js
+++ app.js
@@ -2,16 +2,43 @@
 
 function mostrarResultado () {
     const nome = document.getElementById(`nome`).value
-    const altura = document.getElementById(`altura`).value
-    const peso = document.getElementById(`peso`).value
+    const altura = +document.getElementById(`altura`).value
+    const peso = +document.getElementById(`peso`).value
     const imcForm = document.getElementById(`imc-form`)
+    const resultado = document.getElementById(`result`)
 
-    console.log(imcForm.reportValidity())
+    if (imcForm.reportValidity())
+    {
+        const valorIMC = (peso / altura ** 2).toFixed(2)
+        let classificacao = ''
 
-    console.log(`nome: ${nome}`)
-    console.log(`peso: ${peso}`)
-    console.log(`altura: ${altura}`)
-    
+        if (valorIMC < 18.5)
+        {
+            classificacao = `abaixo do peso.`
+        }
+        else if (valorIMC < 25)
+        {
+            classificacao = `com peso ideal. Parebéns.`
+        }
+        else if (valorIMC < 30)
+        {
+            classificacao = `levemente acima do peso.`
+        }
+        else if (valorIMC < 35)
+        {
+            classificacao = `com obesidade grau I.`
+        }
+        else if (valorIMC < 40)
+        {
+            classificacao = `com obesidade grau II.`
+        }
+        else
+        {
+            classificacao = `com obesidade grau III. Cuidado.`
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC}. e você está ${classificacao}`
+    }
 }
 
 const calcular = document.getElementById(`calcular`).addEventListener(`click`, mostrarResultado)
